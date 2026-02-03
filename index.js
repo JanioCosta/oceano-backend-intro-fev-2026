4b2ceea89236cc74994e3dde3f081e874b7d3f9f
Janio Costa <jancvieira@gmail.com>
2026-02-03 04:32:55 +0000

    Rotas das requisições invertidas e renomeadas

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from 'express'
 
 const app = express()
 
-app.get('/oi', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Olá, mundo!')
 }) // o servidor segue ignorando a segunda requisição e não retorna a 2a resposta.
+// alterada a rota da 1a requisição: definido '/'
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World')
 }) // ordem das requisições invertidas.
+// alterada a rota da segunda requisição: definido '/hello'
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
