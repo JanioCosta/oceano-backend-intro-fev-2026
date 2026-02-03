f9a965edb19a9fde4f09388331a40c4ed5cac06d
Janio Costa <jancvieira@gmail.com>
2026-02-03 04:26:45 +0000

    Ordem das requisições com app.get invertida - primeira  ola mundo - segunda hello world

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
 app.get('/oi', (req, res) => {
   res.send('Olá, mundo!')
 }) // o servidor segue ignorando a segunda requisição e não retorna a 2a resposta.
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+}) // ordem das requisições invertidas.
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
